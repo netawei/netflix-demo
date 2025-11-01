77a01e97dd3d1977045fd931296fa84f4ee30319
NoobMajesty <47846708+NoobMajesty@users.noreply.github.com>
2025-11-01 20:48:54 +0200

    made the content be pulled from the DB rather then hard coded(Still need make centralized URL)

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,56 @@
+/** @format */
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const session = require('express-session');
+const session = require("express-session");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const { connectDB } = require("./config/db");
 
-const userRoutes = require('./routes/userRoutes');
-const contentRoutes = require('./routes/contentRoutes');
+const userRoutes = require("./routes/userRoutes");
+const contentRoutes = require("./routes/contentRoutes");
 const watchHistoryRoutes = require("./routes/watchHistoryRoutes");
+const cors = require("cors");
 
 dotenv.config();
 
 // Disable SSL verification for self-signed certificates (for development only)
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 // Set Mongoose strictQuery option to avoid deprecation warning
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 const app = express();
+
+// allow our local frontend to access backend api
+app.use(
+	cors({
+		origin: "*", // your frontend address
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	})
+);
 
 // Connect Database
 connectDB();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(session({
-  secret: process.env.COOKIE_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
-}));
+app.use(
+	session({
+		secret: process.env.COOKIE_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: { secure: false },
+	})
+);
 
-
-app.use('/api/users', userRoutes);
-app.use('/api/content', contentRoutes);
-app.use('/api/watchHistory', watchHistoryRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/content", contentRoutes);
+app.use("/api/watchHistory", watchHistoryRoutes);
 
 // Server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
